19b148cefebf25cb1bd0a742428dce43a475fb91
Manuell91 <34925152+Manuell91@users.noreply.github.com>
2023-12-02 00:05:00 +0200

    Initialize new Repo.

---
+++ src/main/java/com/example/universitysoftwareengineeringproject/UniversitySoftwareEngineeringProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.universitysoftwareengineeringproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UniversitySoftwareEngineeringProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UniversitySoftwareEngineeringProjectApplication.class, args);
+    }
+    //New Repo
+}
---
+++ src/test/java/com/example/universitysoftwareengineeringproject/UniversitySoftwareEngineeringProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.universitysoftwareengineeringproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UniversitySoftwareEngineeringProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
