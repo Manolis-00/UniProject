3fc8862fafb3ace87f27555d6983c4b803fd0551
Manuell91 <34925152+Manuell91@users.noreply.github.com>
2024-02-19 22:00:43 +0200

    feature: basic structure of the BankAccountService. Needs more work, and even more design.

---
+++ src/main/java/com/project/service/bank/BankAccountService.java
@@ -0,0 +1,22 @@
+package com.project.service.bank;
+
+import com.project.model.bank.BankAccount;
+import org.springframework.stereotype.Service;
+
+import java.math.BigDecimal;
+
+@Service
+public class BankAccountService {
+
+    public void deposit(BigDecimal amount) {
+        //TODO - Add logic
+    }
+
+    public void withdraw(BigDecimal amount) {
+        //TODO - Add logic
+    }
+
+    public void transfer(BigDecimal amount, BankAccount targetAccount) {
+        //TODO - Add logic
+    }
+}
