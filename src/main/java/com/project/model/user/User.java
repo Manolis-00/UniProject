823780762cbcfa5c53aaada94dac06cc71ce17df
Manuell91 <34925152+Manuell91@users.noreply.github.com>
2024-02-19 22:40:59 +0200

    feature: completed changes to the User.

--- src/main/java/com/project/model/user/User.java
+++ src/main/java/com/project/model/user/User.java
@@ -1,15 +1,30 @@
 package com.project.model.user;
 
 import com.project.model.impl.AccountCredentials;
+import com.project.model.impl.Address;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
+import java.time.LocalDateTime;
+import java.util.Set;
+
+@AllArgsConstructor
+@NoArgsConstructor
 @Getter
 @Setter
 @Entity
 @Table(name="user")
+@EntityListeners(AuditingEntityListener.class)
 public class User {
 
     @Id
@@ -17,32 +32,45 @@ public class User {
     @Column(name="social_security_number")
     private String socialSecurityNumber;
 
-    @NotNull
-    @Column(name = "first_name")
+    @Size(min = 1, max = 255, message = "The first name must not be null or empty and must be at most 255 characters.")
+    @Column(name = "first_name", nullable = false)
     private String firstName;
 
-    @NotNull
-    @Column(name="lastName")
+    @Size(min = 1, max = 255, message = "Last name must not be null or empty, and must be at most 255 characters.")
+    @Column(name="lastName", nullable = false)
     private String lastName;
 
-    @NotNull
-    @Column(name="email")
+    @Size(min = 1, max = 255, message = "Email must not be null or empty, and must be at most 255 characters.")
+    @Column(name="email", nullable = false)
     private String email;
 
-    @NotNull
-    @Column(name="phone_number")
+    @Size(min = 10, max = 10, message = "Phone number must have precisely 10 digits.")
+    @Column(name="phone_number", nullable = false)
     private Long phoneNumber;
 
-    @NotNull
-    @Column(name = "address")
-    private String address; //TODO make an addressId class.
+    @OneToMany
+    @JoinColumn(name="address_id")
+    @Column(name = "address", nullable = false)
+    private Set<Address> addresses;
 
     @NotNull
-    @Column(name = "user_role")
+    @Column(name = "user_role", nullable = false)
     private UserRole userRole;
 
     @Column(name = "account_credentials", nullable = false)
     @OneToOne
-    @JoinColumn(name = "username")
+    @JoinColumn(name = "username", nullable = false)
     private AccountCredentials accountCredentials;
+
+    @CreatedBy
+    private String createdBy;
+
+    @CreatedDate
+    private LocalDateTime createdDate;
+
+    @LastModifiedBy
+    private String lastModifiedBy;
+
+    @LastModifiedDate
+    private LocalDateTime lastModifiedDate;
 }
