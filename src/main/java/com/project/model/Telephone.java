fd39efd733f9d2e285601ac7a9295a7c3a57aedd
Manuell91 <34925152+Manuell91@users.noreply.github.com>
2024-02-20 23:34:05 +0200

    feature: created the Telephone entity.

---
+++ src/main/java/com/project/model/Telephone.java
@@ -0,0 +1,52 @@
+package com.project.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import jakarta.validation.constraints.Size;
+import org.hibernate.annotations.Generated;
+import org.hibernate.annotations.GenerationTime;
+import org.hibernate.annotations.UuidGenerator;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Entity
+@Table(name="telephones")
+@EntityListeners(AuditingEntityListener.class)
+public class Telephone {
+
+    @Id
+    @GeneratedValue(generator = "UUID")
+    @UuidGenerator(style = UuidGenerator.Style.AUTO)
+    private UUID id;
+
+    @Size(max =  15, message = "The telephone number must be at most 15 characters")
+    private String number;
+
+    @Size(max =  50, message = "The type must not exceed 50 characters")
+    private String type;
+
+    @CreatedBy
+    private String createdBy;
+
+    @CreatedDate
+    private LocalDateTime createdDate;
+
+    @LastModifiedBy
+    private String lastModifiedBy;
+
+    @LastModifiedDate
+    private LocalDateTime lastModifiedDate;
+}
